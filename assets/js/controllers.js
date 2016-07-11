e28fdb9c7736473fc2b281e3d070d9d83425c226
TED <tedlade.tl@gmail.com>
2016-07-11 15:53:02 +0100

    Testing Facebook share

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers', ['ngAnimate'])
   
-.controller('appCtrl', function($scope, $state, $stateParams, Restangular, $location, $anchorScroll, $timeout) {
+.controller('appCtrl', function($scope, $state, $stateParams, Restangular, $location, $anchorScroll, $timeout, anchorSmoothScroll) {
     Restangular.all('medicine').getList().then(function(response){
         $scope.medicines = _.uniq(response, function(o){
          return o.name
@@ -12,9 +12,10 @@ angular.module('app.controllers', ['ngAnimate'])
         $scope.showResults = true;
     }
     
-	$scope.scrollTo = function(id) {
-		$location.hash(id);
-	    $anchorScroll();
+	$scope.scrollTo = function(eID) {
+		$location.hash('main');
+        anchorSmoothScroll.scrollTo(eID);
+	    // $anchorScroll();
 	};
 
 	$scope.select = function(med) {
@@ -36,12 +37,46 @@ angular.module('app.controllers', ['ngAnimate'])
         $scope.judgement = Math.round($scope.answer);
     	$scope.two = true;
         $scope.personNode = true
-        $timeout(function(){$scope.loaded = true}, 2000);
+        $scope.loaded = true
+        // $timeout(function(){$scope.loaded = true}, 2000);
         
     }
+
+    // $scope.share = function() {
+    //     FB.ui({
+    //         method: 'feed',
+    //         name: 'At &#8358 $scope.credentials.price, we are paying $scope.judgement percent of the global average price for $scope.credentials.name',
+    //         link: 'http://medicine-prices.herokuapp.com',
+    //         picture: '',
+    //         caption: '',
+    //         description: 'This is the content of the "description" field, below the caption.',
+    //         message: ''
+    //     });
+    // }
 
     $scope.close = function() {
         $scope.personNode = false;
         $state.reload();
     }
 })
+
+.directive('fbShare', [
+    function() {
+        return {
+            restrict: 'A',
+            link: function(scope, element) {
+                element.on('click', function() {
+                    FB.ui({
+                        method: 'feed',
+                        name: 'Name you want to show',
+                        link: 'http://link-you-want-to-show',
+                        picture: 'http://picture-you-want-to-show',
+                        caption: 'Caption you want to show',
+                        description: 'Description you want to show',
+                        message: 'Message you want to show'
+                    });
+                });
+            }
+        };
+    }
+]);
